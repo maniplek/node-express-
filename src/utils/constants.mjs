f9653ef66d03430a828907de57886e817b622c8e
Maniple kwizera <maniple.kwizera@codeland.it>
2024-04-02 16:08:33 +0200

    Add Routes

---
+++ src/utils/constants.mjs
@@ -0,0 +1,10 @@
+export const mockUsers = [
+    { id: 1, name: 'Wagon', displayName: 'W' },
+    { id: 2, name: 'Ben', displayName: 'B' },
+    { id: 3, name: 'Cela', displayName: 'C' },
+    { id: 4, name: 'Frank', displayName: 'F' },
+    { id: 5, name: 'Dan', displayName: 'D' },
+    { id: 6, name: 'Melo', displayName: 'M' },
+    { id: 7, name: 'Amaly', displayName: 'A' },
+    { id: 8, name: 'Harry', displayName: 'H' },
+  ];
